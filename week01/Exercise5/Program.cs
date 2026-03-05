65d71717bb1f5f12d3314737dca169ebdb834e17
8-20 <scherbelpa@gmail.com>
2026-03-04 17:19:57 -1000

    Completed week01 exercise5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        DisplayMessage();
+
+        string userName = PromptUserName();
+
+        int favoriteNumber = PromptFavoriteNumber();
+
+        int squaredNumber = SquareNumber(favoriteNumber);
+ 
+        DisplayResult(userName, favoriteNumber, squaredNumber);
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the Program");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptFavoriteNumber()
+        {
+            Console.Write("What is your favorite number? ");
+            int favoriteNumber = int.Parse(Console.ReadLine());
+            return favoriteNumber;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+
+        static void DisplayResult(string userName, int favoriteNumber, int squaredNumber)
+        {
+            Console.WriteLine($"{userName}, the square of your favorite number, {favoriteNumber}, is {squaredNumber}.");
+        }
+
     }
 }
+
+
+
