889815e9604da4c2e1b3bfe8d12063aecbb0f296
8-20 <scherbelpa@gmail.com>
2026-03-19 11:53:25 -1000

    Added Creativity Description to Journal Program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿// Creative Enhancements
+// 1. Added a few of my own prompts
+// 2. Added a third class: UserSelection.cs
+// 3. Added a way for the user to add additional comments in addition to the random prompt
+// 4. By default the working directory on my computer is several levels below the source code 
+//    directory. I added code to store the user's journal in the same directory as the
+//    source code.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 
